e96f534fffd5397df6ea968b155c07299771491b
Mitul Sartanpara <mitulsartanpara0607@gmail.com>
2021-09-28 11:26:23 +0530

    header completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/Navbar'
-import { Fragment } from 'react'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
-function App () {
+
+function App() {
   return (
-    <Fragment>
+    <>
       <Navbar />
-    </Fragment>
-  )
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
